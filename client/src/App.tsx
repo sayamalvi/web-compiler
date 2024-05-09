adbaae6f93441abc685cd1d868f3dae32bd7a2a7
Sayam Alvi <sayamalvi07@gmail.com>
2024-05-09 20:34:05 +0530

    frontend init

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+const App = () => {
+  return (
+    <div className="text-2xl">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App 
